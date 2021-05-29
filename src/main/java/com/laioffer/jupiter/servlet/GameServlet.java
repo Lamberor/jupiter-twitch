0fef5d4b95cf3e303fa3f87abaff6b1bfe089f8c
Qiuyang <lamberor@gmail.com>
2021-05-29 22:30:13 +0800

    Implement GameServlet with JSON and Jackson

--- src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -1,5 +1,10 @@
 package com.laioffer.jupiter.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.entity.Game;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,11 +14,41 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        Game.Builder builder = new Game.Builder();
+        builder.setName("World of Warcraft");
+        builder.setDeveloper("Blizzard Entertainment");
+        builder.setReleaseTime("Feb 11, 2005");
+        builder.setWebsite("https://www.worldofwarcraft.com");
+        builder.setPrice(49.99);
+
+        Game game = builder.build();
+        response.getWriter().print(mapper.writeValueAsString(game));
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Read game information from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String name = jsonRequest.getString("name");
+        String developer = jsonRequest.getString("developer");
+        String releaseTime = jsonRequest.getString("release_time");
+        String website = jsonRequest.getString("website");
+        float price = jsonRequest.getFloat("price");
 
+        //Print game information to IDE console
+        System.out.println("Name is: " + name);
+        System.out.println("Developer is: " + developer);
+        System.out.println("Release time is: " + releaseTime);
+        System.out.println("Website is: " + website);
+        System.out.println("Price is: " + price);
+
+        //Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 }
