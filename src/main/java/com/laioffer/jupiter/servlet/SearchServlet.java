0f8fdb9f3a8e75f5b0b254d574ae5236f4198a1f
Qiuyang <lamberor@gmail.com>
2021-06-05 17:58:23 +0800

    Implement functions for searching streams/clips/videos

---
+++ src/main/java/com/laioffer/jupiter/servlet/SearchServlet.java
@@ -0,0 +1,30 @@
+package com.laioffer.jupiter.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.external.TwitchClient;
+import com.laioffer.jupiter.external.TwitchException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SearchServlet", value = "/search")
+public class SearchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String gameId = request.getParameter("game_id");
+        if (gameId == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        TwitchClient client = new TwitchClient();
+        try {
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchItems(gameId)));
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+    }
+}
